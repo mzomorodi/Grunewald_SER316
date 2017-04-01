612d16983b623f7556e3c2b34ca41708ea31ecb8
Matthew Seiler <mdseiler@asu.edu>
2017-04-01 15:08:05 -0700

    Place calendar day numbers in top left of each cell on calendar

--- src/net/sf/memoranda/ui/JNCalendar.java
+++ src/net/sf/memoranda/ui/JNCalendar.java
@@ -14,6 +14,7 @@ import java.util.Vector;
 
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
@@ -50,6 +51,8 @@ public class JNCalendar extends JTable {
 		/* table properties */
 		setCellSelectionEnabled(true);
 		setRowHeight(100);
+		
+		renderer.setVerticalAlignment(SwingConstants.TOP);
 		
 		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		getTableHeader().setReorderingAllowed(false);
