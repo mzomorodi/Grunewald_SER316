8c6f21437a7a7be712352850ffe39df17e41f7a8
Matthew Seiler <mdseiler@asu.edu>
2017-04-12 21:12:27 -0700

    Task 36: add initial commit for notes FileImport unit test

---
+++ src/net/sf/memoranda/util/FileImportTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package net.sf.memoranda.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Matthew Seiler
+ *
+ */
+public class FileImportTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
